7e3da1d484e966719da19ed21795c531d35f9d20
Yee <13137880636@163.com>
2018-04-13 18:13:48 +0800

    Fix

---
+++ BadgeViewDemo/BadgeViewDemo/YeeBadgeView/YeeBadgeViewHeader.h
@@ -0,0 +1,17 @@
+//
+//  YeeBadgeViewHeader.h
+//  BadgeViewDemo
+//
+//  Created by Yee on 2018/4/13.
+//  Copyright © 2018年 Yee. All rights reserved.
+//
+
+#ifndef YeeBadgeViewHeader_h
+#define YeeBadgeViewHeader_h
+
+#import "YeeBadgeView.h"
+#import "UIView+YeeBadge.h"
+#import "UIBarButtonItem+YeeBadge.h"
+#import "UITabBarItem+YeeBadge.h"
+
+#endif /* YeeBadgeViewHeader_h */
